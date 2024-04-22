20af398b62fa6eded056f50212412f10cae6ce25
suraj <surajj3404@gmail.com>
2024-04-22 07:29:33 +0530

    add light client store

--- eth-lc/src/lib.rs
+++ eth-lc/src/lib.rs
@@ -1,3 +1,29 @@
+use ssz_rs::prelude::*;
 
+use crate::types::*;
 
 pub mod types;
+
+#[derive(Debug, Default, SimpleSerialize)]
+pub struct LightClientStore {
+    pub finalized_header: Header,
+    pub current_sync_committee: SyncCommittee,
+    pub next_sync_committee: Option<SyncCommittee>,
+    pub optimistic_header: Header,
+    pub previous_max_active_participants: u64,
+    pub current_max_active_participants: u64,
+}
+
+impl LightClientStore {
+    pub fn init(bootstrap: Bootstrap, _trusted_block_root: Bytes32) -> Self {
+        // verify if the root is in sync with the bootstrap
+        LightClientStore {
+            finalized_header: bootstrap.header.clone(),
+            current_sync_committee: bootstrap.current_sync_committee,
+            next_sync_committee: None,
+            optimistic_header: bootstrap.header.clone(),
+            previous_max_active_participants: 0,
+            current_max_active_participants: 0,
+        }
+    }
+}
